5c0fe237a63a7047659bc1fb6053795e9aa434d0
Mohamed abdikarim <moi-ali@hotmail.com>
2024-06-21 15:44:54 +0000

    added post page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import styles from "./App.module.css";
+
+import React from "react";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
+ 
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className="App">
       <NavBar />
-      <Container className={styles.Main}>
+      <Container className="Main">
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/signin" component={SignInForm} />
+          <Route exact path="/signup" component={SignUpForm} />
+          <Route exact path="/posts/create" component={PostCreateForm} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>

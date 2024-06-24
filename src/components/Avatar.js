b8d1a0e0568b0b5a21d819ca4e93af6308752c0b
Mohamed abdikarim <moi-ali@hotmail.com>
2024-06-24 14:49:59 +0000

    added

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
-function Avatar({ src, height = 45, text }) {
+const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
       <img
         className={styles.Avatar}
-        alt="avatar"
         src={src}
         height={height}
         width={height}
+        alt="avatar"
       />
       {text}
     </span>
   );
-}
+};
 
 export default Avatar;
